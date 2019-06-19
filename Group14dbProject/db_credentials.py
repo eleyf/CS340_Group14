085153221d90f58dd83d3658aa45ac8009f74680
eleyf <eleyf@oregonstate.edu>
2019-06-19 04:53:53 -0500

    Update db_credentials.py

--- Group14dbProject/db_credentials.py
+++ Group14dbProject/db_credentials.py
@@ -4,7 +4,7 @@
 # the following will be used by the db_connector.py and 
 # in turn by all the Python code in this codebase to interact with the database
 
-host = 'classmysql.engr.oregonstate.edu'  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
-user = 'cs340_eleyf' #CHANGE ME
-passwd = '3352' #CHANGE ME
-db = 'cs340_eleyf' #CHANGE ME
+host = ''  #DON'T CHANGE ME UNLESS THE INSTRUCTIONS SAY SO
+user = '' #CHANGE ME
+passwd = '' #CHANGE ME
+db = '' #CHANGE ME
